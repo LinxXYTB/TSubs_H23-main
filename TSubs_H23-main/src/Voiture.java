6129de7e92527438e67e87a0bdfdfd24a13f60f1
Iulian Moraru <iulianghitiu0102@gmail.com>
2023-05-29 18:43:57 -0400

    TODO 5

--- TSubs_H23-main/src/Voiture.java
+++ TSubs_H23-main/src/Voiture.java
@@ -9,5 +9,16 @@ class Voiture extends Vehicule {
     }
 
     // Getter et setter
+    public void setNombrePortes(int nombrePortes) {
+        this.nombrePortes = nombrePortes;
+    }
+    public int getNombrePortes() {
+        return nombrePortes;
+    }
     //TODO 5
+    @Override
+    public String toString() {
+        return "Marque= " + super.getMarque() + ", Modele= " + super.getModele() + ", Annee= " + super.getAnnee() + ", Nombre portes= " + nombrePortes;
+    }
+
 }
