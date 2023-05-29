a84434650ce891c0227217c6b29798dffa2f6150
Iulian Moraru <iulianghitiu0102@gmail.com>
2023-05-29 17:16:28 -0400

    TODO 1

--- TSubs_H23-main/src/Chat.java
+++ TSubs_H23-main/src/Chat.java
@@ -1,2 +1,29 @@
 // Classe dérivée
 // TODO 1.2
+class Chat extends Animal {
+    private boolean estInterieur;
+    
+    public Chat(String nom, int age, boolean estInterieur) {
+        super(nom, age);
+        this.estInterieur = estInterieur;
+    }
+
+    @Override
+    public String toString() {
+        if (estInterieur) {
+            return super.toString().replaceAll("]", "") + ", est interieur ]";
+        }else{
+            return super.toString().replaceAll("]", "") + ", n'est pas interieur ]";
+        }
+    }
+
+    public void faireDuBruit() {
+        System.out.print("woof woof");
+    }
+    public boolean getInterieur(){
+        return estInterieur;
+    }
+    public void setInterieur(boolean estInterieur) {
+        this.estInterieur = estInterieur;
+    }
+}
--- TSubs_H23-main/src/Chien.java
+++ TSubs_H23-main/src/Chien.java
@@ -1,2 +1,25 @@
 // Classe dérivée
 // TODO 1.1
+class Chien extends Animal {
+    private String race;
+    
+    public Chien(String nom, int age, String race) {
+        super(nom, age);
+        this.race = race;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replaceAll("]", "") + ", race=" + race + " ]";
+    }
+
+    public void faireDuBruit() {
+        System.out.print("woof woof");
+    }
+    public String getRace() {
+        return race;
+    }
+    public void setRace(String race) {
+        this.race = race;
+    }
+}
